d90019fd48b9f3a6e38287c5fbb08bc26f497ba4
beeble-jisoolee <jisoolee0123@gmail.com>
2023-09-15 15:11:06 +0900

    add autoupdater

--- main/background.ts
+++ main/background.ts
@@ -1,40 +1,100 @@
-import { app } from 'electron'
-import serve from 'electron-serve'
-import { createWindow } from './helpers'
+import { app, dialog, autoUpdater } from "electron";
+// import { autoUpdater, UpdateInfo } from "electron-updater";
+import * as log from "electron-log";
+import serve from "electron-serve";
+import { createWindow } from "./helpers";
 
-const isProd: boolean = process.env.NODE_ENV === 'production'
-
-// require('update-electron-app')()
-
-// import updateElectronApp from 'update-electron-app'
-// updateElectronApp()
-
-const updateElectronApp: () => void = require('update-electron-app')
-updateElectronApp()
+const isProd: boolean = process.env.NODE_ENV === "production";
 
 if (isProd) {
-  serve({ directory: 'app' })
+  serve({ directory: "app" });
 } else {
-  app.setPath('userData', `${app.getPath('userData')} (development)`)
+  app.setPath("userData", `${app.getPath("userData")} (development)`);
 }
 
-;(async () => {
-  await app.whenReady()
+let updateInterval = null;
 
-  const mainWindow = createWindow('main', {
+// autoUpdater.on("checking-for-update", () => {
+//   log.info("업데이트 확인 중...");
+// });
+// autoUpdater.on("update-not-available", (info: any) => {
+//   log.info("현재 최신버전입니다.");
+// });
+// autoUpdater.on("error", (err) => {
+//   log.info("에러가 발생하였습니다. 에러내용 : " + err);
+// });
+// autoUpdater.on("update-downloaded", (info: any) => {
+//   log.info("업데이트가 완료되었습니다.");
+// });
+
+// autoUpdater.on("update-available", (_event, releaseNotes, releaseName) => {
+//   const dialogOpts = {
+//     type: "info",
+//     buttons: ["Ok"],
+//     title: "Update Available",
+//     message: process.platform === "win32" ? releaseNotes : releaseName,
+//     detail:
+//       "A new version download started. The app will be restarted to install the update.",
+//   };
+//   dialog.showMessageBox(dialogOpts);
+
+//   updateInterval = null;
+// });
+
+// autoUpdater.on("update-downloaded", (_event, releaseNotes, releaseName) => {
+//   const dialogOpts = {
+//     type: "info",
+//     buttons: ["Restart", "Later"],
+//     title: "Application Update",
+//     message: process.platform === "win32" ? releaseNotes : releaseName,
+//     detail:
+//       "A new version has been downloaded. Restart the application to apply the updates.",
+//   };
+//   dialog.showMessageBox(dialogOpts).then((returnValue) => {
+//     if (returnValue.response === 0) autoUpdater.quitAndInstall();
+//   });
+// });
+
+const data = {
+  provider: "github",
+  owner: "beeble-ai",
+  repo: "desktop-temp",
+  protocol: "https",
+};
+
+// autoUpdater.setFeedURL(data as any);
+
+(async () => {
+  await app.whenReady();
+
+  const mainWindow = createWindow("main", {
     width: 1000,
     height: 600,
-  })
+  });
 
   if (isProd) {
-    await mainWindow.loadURL('app://./home.html')
+    await mainWindow.loadURL("app://./home.html");
   } else {
-    const port = process.argv[2]
-    await mainWindow.loadURL(`http://localhost:${port}/home`)
-    mainWindow.webContents.openDevTools()
+    const port = process.argv[2];
+    await mainWindow.loadURL(`http://localhost:${port}/home`);
+    mainWindow.webContents.openDevTools();
   }
-})()
 
-app.on('window-all-closed', () => {
-  app.quit()
-})
+  const server = "https://update.electronjs.org";
+  const feed = `${server}/beeble-ai/desktop-temp/${process.platform}/1.0.0`;
+  // }/${app.getVersion()}`;
+
+  // autoUpdater.setFeedURL(feed as any);
+  // autoUpdater.checkForUpdates();
+  // updateInterval = setInterval(() => {
+  //   autoUpdater.checkForUpdates();
+  //   console.log("checking...");
+  //   console.log(app.getVersion());
+  //   console.log(process.platform, process.arch, app.getVersion());
+  //   console.log(autoUpdater.getFeedURL());
+  // }, 60 * 1000);
+})();
+
+app.on("window-all-closed", () => {
+  app.quit();
+});

18150dad6f949c8bd6926ed4e3eb4c6ae4991e74
Wonjun Yoon <wonjun.yoon@beeble.ai>
2023-09-15 15:50:27 +0900

    gitignore updated

--- main/background.ts
+++ main/background.ts
@@ -17,43 +17,43 @@ let updateInterval = null;
 autoUpdater.on("checking-for-update", () => {
   log.info("업데이트 확인 중...");
 });
-// autoUpdater.on("update-not-available", (info: any) => {
-//   log.info("현재 최신버전입니다.");
-// });
-// autoUpdater.on("error", (err) => {
-//   log.info("에러가 발생하였습니다. 에러내용 : " + err);
-// });
-// autoUpdater.on("update-downloaded", (info: any) => {
-//   log.info("업데이트가 완료되었습니다.");
-// });
+autoUpdater.on("update-not-available", (info: any) => {
+  log.info("현재 최신버전입니다.");
+});
+autoUpdater.on("error", (err) => {
+  log.info("에러가 발생하였습니다. 에러내용 : " + err);
+});
+autoUpdater.on("update-downloaded", (info: any) => {
+  log.info("업데이트가 완료되었습니다.");
+});
 
-// autoUpdater.on("update-available", (_event, releaseNotes, releaseName) => {
-//   const dialogOpts = {
-//     type: "info",
-//     buttons: ["Ok"],
-//     title: "Update Available",
-//     message: process.platform === "win32" ? releaseNotes : releaseName,
-//     detail:
-//       "A new version download started. The app will be restarted to install the update.",
-//   };
-//   dialog.showMessageBox(dialogOpts);
+autoUpdater.on("update-available", (_event, releaseNotes, releaseName) => {
+  const dialogOpts = {
+    type: "info",
+    buttons: ["Ok"],
+    title: "Update Available",
+    message: process.platform === "win32" ? releaseNotes : releaseName,
+    detail:
+      "A new version download started. The app will be restarted to install the update.",
+  };
+  dialog.showMessageBox(dialogOpts).then((returnValue) => {
+    log.info("LOG: " + returnValue.response)
+    if (returnValue.response === 0) autoUpdater.quitAndInstall();
+  });
 
-//   updateInterval = null;
-// });
+  updateInterval = null;
+});
 
-// autoUpdater.on("update-downloaded", (_event, releaseNotes, releaseName) => {
-//   const dialogOpts = {
-//     type: "info",
-//     buttons: ["Restart", "Later"],
-//     title: "Application Update",
-//     message: process.platform === "win32" ? releaseNotes : releaseName,
-//     detail:
-//       "A new version has been downloaded. Restart the application to apply the updates.",
-//   };
-//   dialog.showMessageBox(dialogOpts).then((returnValue) => {
-//     if (returnValue.response === 0) autoUpdater.quitAndInstall();
-//   });
-// });
+autoUpdater.on("update-downloaded", (_event, releaseNotes, releaseName) => {
+  const dialogOpts = {
+    type: "info",
+    buttons: ["Restart", "Later"],
+    title: "Application Update",
+    message: process.platform === "win32" ? releaseNotes : releaseName,
+    detail:
+      "A new version has been downloaded. Restart the application to apply the updates.",
+  };
+});
 
 (async () => {
   await app.whenReady();
@@ -72,11 +72,10 @@ autoUpdater.on("checking-for-update", () => {
   }
 
   const server = "https://update.electronjs.org";
-  const feed = `${server}/beeble-ai/desktop-temp/${process.platform}/1.0.0`;
-  // }/${app.getVersion()}`;
+  const feed = `${server}/beeble-ai/desktop-temp/${process.platform}/${app.getVersion()}`;
 
   autoUpdater.setFeedURL(feed as any);
-  // autoUpdater.checkForUpdates();
+  autoUpdater.checkForUpdates();
   updateInterval = setInterval(() => {
     autoUpdater.checkForUpdates();
     console.log("checking...");
